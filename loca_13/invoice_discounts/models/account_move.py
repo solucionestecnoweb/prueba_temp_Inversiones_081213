f99b20e42714af2a27310bf9a50aebe7b527511c
bgomez31 <bryan.gomez1311@gmail.com>
2021-10-14 22:02:11 -0400

    [MOD] se genera descuentos automaticos

--- loca_13/invoice_discounts/models/account_move.py
+++ loca_13/invoice_discounts/models/account_move.py
@@ -5,25 +5,146 @@ from odoo import api, fields, models
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+    # Descuento
     is_discount = fields.Boolean(string='Tiene Descuento')
     discount5 = fields.Float(string="Descuento 1")
     discount3 = fields.Float(string="Descuento 2")
     discount2 = fields.Float(string="Descuento 3")
 
-    @api.onchange('discount5')
-    def _onchange_discount5_invoice(self):
-        if self.discount5 > 0:
-            disc5 = (self.amount_total * self.discount5) / 100
-            self.amount_total = disc5
+    #Cantidad por Descuento
+    am_discount1 = fields.Float(string="Descuento #1", compute="_compute_discount_amount_total", store=True)
+    am_discount2 = fields.Float(string="Descuento #2", compute="_compute_discount_amount_total", store=True)
+    am_discount3 = fields.Float(string="Descuento #3", compute="_compute_discount_amount_total", store=True)
 
-    @api.onchange('discount3')
-    def _onchange_discount3_invoice(self):
-        if self.discount3 > 0:
-            disc3 = (self.amount_total * self.discount3) / 100
-            self.amount_total = disc3
+    def _compute_discount_amount_total(self):
+        for move in self:
+            if move.discount5 > 0.0:
+                disc5 = (move.amount_total * move.discount5) / 100
+                rest = (move.amount_total - disc5)
+                move.am_discount1 = disc5
+                move.update({'amount_total': rest})
+            if move.discount3 > 0.0:
+                disc3 = (move.amount_total * move.discount3) / 100
+                rest = (move.amount_total - disc3)
+                move.am_discount2 = disc3
+                move.update({'amount_total': rest})
+            if move.discount3 > 0.0:
+                disc2 = (move.amount_total * move.discount2) / 100
+                rest = (move.amount_total - disc2)
+                move.am_discount3 = disc2
+                move.update({'amount_total': rest})
 
-    @api.onchange('discount2')
-    def _onchange_discount2_invoice(self):
-        if self.discount3 > 0:
-            disc2 = (self.amount_total * self.discount2) / 100
-            self.amount_total = disc2
+    @api.depends(
+        'line_ids.debit',
+        'line_ids.credit',
+        'line_ids.currency_id',
+        'line_ids.amount_currency',
+        'line_ids.amount_residual',
+        'line_ids.amount_residual_currency',
+        'line_ids.payment_id.state',
+        'discount5',
+        'discount3',
+        'discount2'
+    )
+    def _compute_amount(self):
+        invoice_ids = [move.id for move in self if move.id and move.is_invoice(include_receipts=True)]
+        self.env['account.payment'].flush(['state'])
+        if invoice_ids:
+            self._cr.execute(
+                '''
+                    SELECT move.id
+                    FROM account_move move
+                    JOIN account_move_line line ON line.move_id = move.id
+                    JOIN account_partial_reconcile part ON part.debit_move_id = line.id OR part.credit_move_id = line.id
+                    JOIN account_move_line rec_line ON
+                        (rec_line.id = part.debit_move_id AND line.id = part.credit_move_id)
+                    JOIN account_payment payment ON payment.id = rec_line.payment_id
+                    JOIN account_journal journal ON journal.id = rec_line.journal_id
+                    WHERE payment.state IN ('posted', 'sent')
+                    AND journal.post_at = 'bank_rec'
+                    AND move.id IN %s
+                UNION
+                    SELECT move.id
+                    FROM account_move move
+                    JOIN account_move_line line ON line.move_id = move.id
+                    JOIN account_partial_reconcile part ON part.debit_move_id = line.id OR part.credit_move_id = line.id
+                    JOIN account_move_line rec_line ON
+                        (rec_line.id = part.credit_move_id AND line.id = part.debit_move_id)
+                    JOIN account_payment payment ON payment.id = rec_line.payment_id
+                    JOIN account_journal journal ON journal.id = rec_line.journal_id
+                    WHERE payment.state IN ('posted', 'sent')
+                    AND journal.post_at = 'bank_rec'
+                    AND move.id IN %s
+                ''', [tuple(invoice_ids), tuple(invoice_ids)]
+            )
+            in_payment_set = set(res[0] for res in self._cr.fetchall())
+        else:
+            in_payment_set = {}
+
+        for move in self:
+            total_untaxed = 0.0
+            total_untaxed_currency = 0.0
+            total_tax = 0.0
+            total_tax_currency = 0.0
+            total_residual = 0.0
+            total_residual_currency = 0.0
+            total = 0.0
+            total_currency = 0.0
+            currencies = set()
+
+            for line in move.line_ids:
+                if line.currency_id:
+                    currencies.add(line.currency_id)
+
+                if move.is_invoice(include_receipts=True):
+                    # === Invoices ===
+
+                    if not line.exclude_from_invoice_tab:
+                        # Untaxed amount.
+                        total_untaxed += line.balance
+                        total_untaxed_currency += line.amount_currency
+                        total += line.balance
+                        total_currency += line.amount_currency
+                    elif line.tax_line_id:
+                        # Tax amount.
+                        total_tax += line.balance
+                        total_tax_currency += line.amount_currency
+                        total += line.balance
+                        total_currency += line.amount_currency
+                    elif line.account_id.user_type_id.type in ('receivable', 'payable'):
+                        # Residual amount.
+                        total_residual += line.amount_residual
+                        total_residual_currency += line.amount_residual_currency
+                else:
+                    # === Miscellaneous journal entry ===
+                    if line.debit:
+                        total += line.balance
+                        total_currency += line.amount_currency
+
+            if move.type == 'entry' or move.is_outbound():
+                sign = 1
+            else:
+                sign = -1
+            move.amount_untaxed = sign * (total_untaxed_currency if len(currencies) == 1 else total_untaxed)
+            move.amount_tax = sign * (total_tax_currency if len(currencies) == 1 else total_tax)
+            move.amount_total = sign * (total_currency if len(currencies) == 1 else total)
+            move._compute_discount_amount_total()
+            move.amount_residual = -sign * (total_residual_currency if len(currencies) == 1 else total_residual)
+            move.amount_untaxed_signed = -total_untaxed
+            move.amount_tax_signed = -total_tax
+            move.amount_total_signed = abs(total) if move.type == 'entry' else -total
+            move.amount_residual_signed = total_residual
+
+            currency = len(currencies) == 1 and currencies.pop() or move.company_id.currency_id
+            is_paid = currency and currency.is_zero(move.amount_residual) or not move.amount_residual
+
+            # Compute 'invoice_payment_state'.
+            if move.type == 'entry':
+                move.invoice_payment_state = False
+            elif move.state == 'posted' and is_paid:
+                if move.id in in_payment_set:
+                    move.invoice_payment_state = 'in_payment'
+                else:
+                    move.invoice_payment_state = 'paid'
+            else:
+                move.invoice_payment_state = 'not_paid'
